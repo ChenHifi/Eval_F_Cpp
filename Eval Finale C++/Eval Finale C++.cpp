c5f0898241bed2f83736d423774243225dcfea0e
Matthieu MARTINEZ <matthieu.martinez@ecole-isitech.fr>
2023-06-15 17:18:12 +0200

    Commit 10

--- Eval Finale C++/Eval Finale C++.cpp
+++ Eval Finale C++/Eval Finale C++.cpp
@@ -5,26 +5,28 @@ using namespace std;
 
 int main() {
     Boxeur boxeur_1("Box_1", 75);
-    std::cout << "Adresse de boxeur_1 : " << &boxeur_1 << ", Nom : " << boxeur_1.getNom() << ", Poids : " << boxeur_1.getPoids() << std::endl;
+    cout << "Adresse de boxeur_1 : " << &boxeur_1 << ", Nom : " << boxeur_1.getNom() << ", Poids : " << boxeur_1.getPoids() << endl;
 
     Boxeur* boxeur_2 = new Boxeur("Box_2", 78);
-    std::cout << "Adresse de boxeur_2 : " << boxeur_2 << ", Nom : " << boxeur_2->getNom() << ", Poids : " << boxeur_2->getPoids() << std::endl;
+    cout << "Adresse de boxeur_2 : " << boxeur_2 << ", Nom : " << boxeur_2->getNom() << ", Poids : " << boxeur_2->getPoids() << endl;
 
     Combat combat_1("Comb_1_1/8");
-    std::cout << "Adresse de combat_1 : " << &combat_1 << ", Niveau : " << combat_1.getNiveau() << std::endl;
+    cout << "Adresse de combat_1 : " << &combat_1 << ", Niveau : " << combat_1.getNiveau() << endl;
 
     combat_1.setCoinBleu(&boxeur_1);
     combat_1.setCoinRouge(boxeur_2);
 
-    std::cout << "Boxeur dans le coin bleu : " << combat_1.getCoinBleu()->getNom() << std::endl;
-    std::cout << "Boxeur dans le coin rouge : " << combat_1.getCoinRouge()->getNom() << std::endl;
+    cout << "Boxeur dans le coin bleu : " << combat_1.getCoinBleu()->getNom() << endl;
+    cout << "Boxeur dans le coin rouge : " << combat_1.getCoinRouge()->getNom() << endl;
+
+    combat_1.DesignerVainqueur("rouge");
 
     Boxeur* vainqueur = combat_1.getVainqueur();
     if (vainqueur != nullptr) {
-        std::cout << "Vainqueur : " << vainqueur->getNom() << std::endl;
+        cout << "Vainqueur : " << vainqueur->getNom() << endl;
     }
     else {
-        std::cout << "Aucun vainqueur désigné." << std::endl;
+        cout << "Aucun vainqueur designe." << endl;
     }
 
     delete boxeur_2;
